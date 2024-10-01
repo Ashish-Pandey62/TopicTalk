134a390c8e915267c4e670cedac4082bd29d07d9
Ashish <ashishpandey2062@gmail.com>
2024-10-01 19:31:34 +0545

    slight _updates : permissions added

--- forum_prj/boards/views.py
+++ forum_prj/boards/views.py
@@ -3,6 +3,7 @@ from .models import Board,Topic,Post
 from django.http import Http404
 from django.contrib.auth.models import User
 from .forms import NewTopicForm
+from django.contrib.auth.decorators import login_required
 
 
 
@@ -16,22 +17,27 @@ def board_topics(request, pk):
     topics = board.topics.all()  
     return render(request, 'boards/topics.html', {'board': board, 'topics': topics})
 
+@login_required
 def new_topic(request, pk):
     board = get_object_or_404(Board, pk=pk)
-    user = User.objects.first()  
     if request.method == 'POST':
         form = NewTopicForm(request.POST)
         if form.is_valid():
             topic = form.save(commit=False)
             topic.board = board
-            topic.starter = user
+            topic.starter = request.user 
             topic.save()
-            post = Post.objects.create(
+            Post.objects.create(
                 message=form.cleaned_data.get('message'),
                 topic=topic,
-                created_by=user
+                created_by=request.user  
             )
-            return redirect('board_topics', pk=board.pk)  # TODO: redirect to the created topic page
+            return redirect('board_topics', pk=board.pk) 
     else:
         form = NewTopicForm()
     return render(request, 'boards/new_topic.html', {'board': board, 'form': form})
+
+
+
+ 
+
